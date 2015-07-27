5c7c09316ac81aaafa3e5c40bcd764cf6031eeb5
aKratzke <arosekratzke@gmail.com>
2015-07-27 17:20:07 -0400

    Added 'AutoController'
    Added the 'AutoController' to the Controllers Master File

---
+++ SuperDealership/Controllers/AutoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperDealership.Controllers
+{
+    public class AutoController : Controller
+    {
+        // GET: Auto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
