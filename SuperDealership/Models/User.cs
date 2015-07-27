2cfe75bc42a26f35171b0f72825d3514df134fa0
aKratzke <arosekratzke@gmail.com>
2015-07-27 16:50:25 -0400

    Created SuperDealership File
    Created File and created User Class

---
+++ SuperDealership/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SuperDealership.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int UserID { get; set; }
+    }
+
+    public partial class AutoDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
